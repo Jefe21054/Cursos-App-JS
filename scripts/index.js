b527adb39b3f69b6ae00108a1274b929c1cf78d0
Jefe21054 <ivaniglesias500@protonmail.ch>
2023-05-14 19:17:34 -0500

    Imprimiendo contenido en el DOM

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,12 @@
 import Curso from './classes/Curso.js'
 
-const html = new Curso('HTML desde cero','https://ed.team/imagenes/html.png',10)
+const html = new Curso('HTML desde cero','https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2F927f5294-45af-4593-a52f-f437bf16d811.png&w=1920&q=75',10)
+const css = new Curso('CSS desde cero','https://ed.team/_next/image?url=https%3A%2F%2Fedteam-media.s3.amazonaws.com%2Fcourses%2Fbig%2F82daef93-44b7-4a0a-9910-42aa609c6eee.png&w=1920&q=75',8)
 
-console.log(html)
+const elem = document.getElementById('curso')
+
+elem.innerHTML = `
+  <img src="${html.getPoster()}"/>
+  <h3>${html.getNombre()}</h3>
+  <span>Cantidad de clases: ${html.getClases()}</span>
+`
